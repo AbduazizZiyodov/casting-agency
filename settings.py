b157487687507eab654fc19248a4e6da2dfc9083
AbduazizZiyodov <abduaziz.ziyodov@mail.ru>
2021-02-14 00:15:38 +0500

    ✅ Define Tokens

--- settings.py
+++ settings.py
@@ -10,6 +10,11 @@ SECRET_KEY = urandom(32) # key => b'+\xf0l\x17+\x971\x1f\xda\xe7\x ...
 DEBUG = True # if production mode => debug equal to false
 
 
+# Get token from enviroment
+ASSISTANT_TOKEN = getenv("ASSISTANT_TOKEN")
+DIRECTOR_TOKEN = getenv("DIRECTOR_TOKEN")
+PRODUCER_TOKEN = getenv("PRODUCER_TOKEN")
+
 api.debug = DEBUG
 api.secret_key = SECRET_KEY
 
